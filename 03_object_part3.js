4eff1b32cff26995b9d452be1bf0338982f18cf6
Anurag pandey <138196853+anuragapandey@users.noreply.github.com>
2024-09-20 02:38:48 +0530

    Add files via upload

---
+++ 03_object_part3.js
@@ -0,0 +1,33 @@
+//node 02_basics/03_basics_part3.js
+
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+//course.courseInstructor
+
+const{courseInstructor:instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+/*const navbar = (pros.company) =>{
+
+},*/
+// const navbar = ({company}) =>{    /// destructure are most used in react
+
+// }
+
+// navbar(company = "hitesh")
+
+/*{
+    "name": "anurag",
+    "coursename": "js in hindi",   called as JSON(transfrom version of excel )
+    "price":"free",
+}*/
+
+
+
+
